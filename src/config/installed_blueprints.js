ceabb40940d2d2bfc6e73620abe808d58ea27b79
Crowdbotics <team@crowdbotics.com>
2020-11-21 16:13:09 +0000

    add package blueprint-react-native-tutorial-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Tutorial177079', human_name: 'Tutorial', access_route: 'Tutorial177079', icon: 'question-circle'},
 { name: 'Maps177078', human_name: 'Maps', access_route: 'Maps177078', icon: 'map'},
 { name: 'CalendarView177077', human_name: 'Calendar View', access_route: 'CalendarView177077', icon: 'calendar'},
 { name: 'Camera177076', human_name: 'Camera', access_route: 'Camera177076', icon: 'camera'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Tutorial177079Navigator from '../features/Tutorial177079/navigator';
 import Maps177078Navigator from '../features/Maps177078/navigator';
 import CalendarView177077Navigator from '../features/CalendarView177077/navigator';
 import Camera177076Navigator from '../features/Camera177076/navigator';
@@ -17,6 +18,7 @@ import EmailAuth177075Navigator from '../features/EmailAuth177075/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Tutorial177079: { screen: Tutorial177079Navigator },
 Maps177078: { screen: Maps177078Navigator },
 CalendarView177077: { screen: CalendarView177077Navigator },
 Camera177076: { screen: Camera177076Navigator },
